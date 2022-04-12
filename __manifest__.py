a9d2b3847d3dc2ed8e565bbf0597d24166232a1d
DesarrolloPtree <alfonsoglzslz@gmail.com>
2022-04-11 21:07:12 -0500

    15.0.0.0.2
    * Add Currency field on CRM Lead
    * Add view on list, form and kanban
    * Add tracking field

---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Smob.io Extension Module",
+
+    'summary': """Smob.io Extension Module""",
+
+    'description': """Smob.io Extension Module""",
+    'author': "Alfonso Gonzalez",
+    'website': "https://ntropy.tech",
+    'category': 'Customizations',
+    'version': '15.0.0.0.2',
+    'license': "AGPL-3",
+    'sequence': "-99",
+    'depends': [
+        'crm',
+    ],
+    'data': [
+        # Security
+        # Views
+        'views/crm_multi_currency.xml',
+        # Data Sample
+
+    ],
+    'images': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
