c81d8752b769626a09b9558da182a0712cf84e3d
DesarrolloPtree <alfonsoglzslz@gmail.com>
2022-05-02 11:05:23 -0500

    icon

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Smob.io Extension Module",
+    'name': "Smob.io - Extension Module",
 
-    'summary': """Smob.io Extension Module""",
+    'summary': """Smob.io - Extension Module""",
 
-    'description': """Smob.io Extension Module""",
+    'description': """Smob.io - Extension Module""",
     'author': "Alfonso Gonzalez",
     'website': "https://ntropy.tech",
     'category': 'Customizations',
     'version': '15.0.0.0.2',
     'license': "AGPL-3",
-    'sequence': "-99",
+    'sequence': "50",
     'depends': [
         'crm',
     ],
